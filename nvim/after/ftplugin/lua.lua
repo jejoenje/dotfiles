078ddc28c4b2be2ebbabf7b4f29b7b3b68d4f6f1
Jeroen Minderman <jejoenje@airthrey.lan>
2022-11-04 18:31:24 +0000

    Building basic lua config from scratch

---
+++ nvim/after/ftplugin/lua.lua
@@ -0,0 +1,4 @@
+-- Appends .lua to allow direct hopping to require() files in init.lua
+vim.opt_local.suffixesadd:prepend('.lua')
+vim.opt_local.suffixesadd:prepend('init.lua')
+vim.opt_local.path:prepend(vim.fn.stdpath('config')..'/lua')
---
+++ nvim/lua/packages.lua
@@ -0,0 +1,9 @@
+require('packer').startup(function()
+  use 'wbthomason/packer.nvim'
+
+  -- Colourschemes
+  use 'tomasr/molokai'
+  use "danilo-augusto/vim-afterglow"
+  use "shaunsingh/nord.nvim"
+  use "arcticicestudio/nord-vim"
+end)
