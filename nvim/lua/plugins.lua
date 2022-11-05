8df8388cc306d2bc215d7dc183250a79a9e5065f
Jeroen Minderman <jejoenje@airthrey.lan>
2022-11-05 10:42:16 +0000

    Set Packer to use a popup window, added pcall to plugins.lua to ensure this works.

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -1,3 +1,18 @@
+-- Use a protected call so we don't error out on first use
+local status_ok, packer = pcall(require, "packer")
+if not status_ok then
+  return
+end
+
+-- Have packer use a popup window
+packer.init {
+  display = {
+    open_fn = function()
+      return require("packer.util").float { border = "rounded" }
+    end,
+  },
+}
+
 require('packer').startup(function()
 
   -- Packer
