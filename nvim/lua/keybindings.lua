1cce157edf0071c43e84a116219fd610b04c5bcd
Jeroen Minderman <jejoenje@airthrey.lan>
2022-11-05 10:36:09 +0000

    Added LSP keybindings

--- nvim/lua/keybindings.lua
+++ nvim/lua/keybindings.lua
@@ -12,3 +12,14 @@ keymap("", "<Space>", "<Nop>", opts)
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 
+keymap('n','gd', ':lua vim.lsp.buf.definition()<cr>',opts)
+keymap('n','gD', ':lua vim.lsp.buf.declaration()<cr>',opts)
+keymap('n','gi', ':lua vim.lsp.buf.implementation()<cr>',opts)
+keymap('n','gw', ':lua vim.lsp.buf.document_symbol()<cr>',opts)
+keymap('n','gw', ':lua vim.lsp.buf.workspace_symbol()<cr>',opts)
+keymap('n','gr', ':lua vim.lsp.buf.references()<cr>',opts)
+keymap('n','gt', ':lua vim.lsp.buf.type_definition()<cr>',opts)
+keymap('n','K', ':lua vim.lsp.buf.hover()<cr>',opts)
+keymap('n','<c-k>', ':lua vim.lsp.buf.signature_help()<cr>',opts)
+keymap('n','<leader>af', ':lua vim.lsp.buf.code_action()<cr>',opts)
+keymap('n','<leader>rn', ':lua vim.lsp.buf.rename()<cr>',opts)
