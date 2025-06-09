7f6a6b75a39a50fdeff4afeeb39811acc727a41a
jejoenje <jeroenminderman@gmail.com>
2025-06-09 19:06:49 +0100

    Changed wal switch script, added to hyprland.conf. Also changed exit shortcut

---
+++ hypr/.config/hypr/scripts/change-wal.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Pick random wallpeper from folder
+wpfile=$(find ~/wallpaper/ -type f -name "*.jpg" -o -name "*.png" -o -name "*.gif" | shuf -n 1)
+
+
+# Reset swww, and set to randomly picked file
+#swww kill
+#swww init
+swww img $wpfile &
+
+# Generate colour scheme
+wal -q -i $wpfile &
+
+
