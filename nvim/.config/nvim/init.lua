66935a97ce140b7809ab65277ebdc5372c1d0be3
Jeroen Minderman <jeroenminderman@gmail.com>
2025-06-23 11:22:37 +0100

    Added initial nvim config steps, copypaste now working but had to install wl-clipboard

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,1 +1,19 @@
-vim.opt.clipboard:append("unnamedplus")            -- Use system clipboard
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
+-- vim.go.have_nerd_font = true
+
+vim.o.number = true
+vim.o.relativenumber = true
+vim.o.cursorline = true
+vim.o.mouse = "a"			-- enable mouse mode
+vim.o.showmode = true			-- show mode
+vim.o.wrap = false                      -- Don't wrap lines
+vim.o.scrolloff = 8                     -- Keep lines above/below cursor 
+vim.o.sidescrolloff = 8                 -- Keep columns left/right of cursor
+
+-- CLIPBOARD ATTEMPT
+-- 
+vim.schedule(function()
+  vim.o.clipboard = "unnamedplus"
+end)
+--vim.o.clipboard = "unnamedplus"
